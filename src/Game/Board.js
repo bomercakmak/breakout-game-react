a3bf8bdd789b4d32f51af3620ee7952e7c9faadf
bomercakmak <bahadir0646@gmail.com>
2021-05-21 21:04:36 +0200

    I added PaddleHitPosition formula.

--- src/Game/Board.js
+++ src/Game/Board.js
@@ -6,10 +6,12 @@ import "../App.css";
 import Paddle from "./Paddle";
 import Brick from "./Brick";
 import BrickCollision from "./util/BrickCollision";
+import PaddleHitPosition from "./util/PaddleHitPosition";
+import PlayerInfo from "./PlayerInfo";
 
 let bricks = [];
 
-let { ballObj, paddleProps,brickObj } = data;
+let { ballObj, paddleProps, brickObj, player } = data;
 
 function Board() {
   const canvasRef = useRef(null);
@@ -17,19 +19,24 @@ function Board() {
     const render = () => {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext("2d");
+      paddleProps.y = canvas.height - 30;
       // Assign Bricks
-      let newBricks = Brick(2,bricks,canvas,brickObj);
-      if(newBricks && newBricks.length > 0){
+
+      let newBricks = Brick(2, bricks, canvas, brickObj);
+      if (newBricks && newBricks.length > 0) {
         bricks = newBricks;
       }
+
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      bricks.map((brick)=> {
-        return brick.draw(ctx)
-      })
+      PlayerInfo(ctx, player, canvas);
+
+      bricks.map((brick) => {
+        return brick.draw(ctx);
+      });
       //Handle Ball Movement
       BallMovement(ctx, ballObj);
       // Ball and Wall Border
-      WallBorder(ballObj, canvas);
+      WallBorder(ballObj, canvas, player,paddleProps);
 
       let brickCollision;
 
@@ -44,11 +51,13 @@ function Board() {
             ballObj.dy *= -1;
             bricks[i].broke = true;
           }
+          player.score += 10;
         }
       }
 
       Paddle(ctx, canvas, paddleProps);
-
+      // Paddle + Ball Collision
+      PaddleHitPosition(ballObj, paddleProps);
       requestAnimationFrame(render);
     };
     render();
@@ -57,7 +66,9 @@ function Board() {
   return (
     <canvas
       id="game-canvas"
-      onMouseMove={(event) => (paddleProps.x = event.clientX- paddleProps.width / 2 - 10)}
+      onMouseMove={(event) =>
+        (paddleProps.x = event.clientX - paddleProps.width / 2 - 10)
+      }
       ref={canvasRef}
       height="500"
       width="800px;"
--- src/Game/util/WallBorder.js
+++ src/Game/util/WallBorder.js
@@ -1,8 +1,13 @@
-function WallBorder(ballObj, canvas) {
-  if (
-    ballObj.y - ballObj.rad <= 0 ||
-    ballObj.y + ballObj.rad >= canvas.height
-  ) {
+function WallBorder(ballObj, canvas, player, paddleProps) {
+  if (ballObj.y + ballObj.rad >= canvas.height) {
+    player.lives--;
+    ballObj.x = paddleProps.x;
+    ballObj.y = paddleProps.y - 30;
+    ballObj.dx = 6 * (Math.random() * 2 - 1);
+    ballObj.dy = -6;
+  }
+
+  if (ballObj.y - ballObj.rad <= 0) {
     ballObj.dy *= -1;
   }
   if (ballObj.x + ballObj.rad >= canvas.width || ballObj.x - ballObj.rad <= 0) {
